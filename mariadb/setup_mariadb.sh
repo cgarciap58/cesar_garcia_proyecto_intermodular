9dc081aa8b4754114335c723bb5f1579ad85855d
César García <cgarciap58@iesalbarregas.es>
2026-04-13 13:23:18 +0200

    Simplificacion y debugeo de arranque de BBDD

--- mariadb/setup_mariadb.sh
+++ mariadb/setup_mariadb.sh
@@ -1,13 +1,36 @@
-# https://medium.com/code-zen/django-mariadb-85cc9daeeef8
+# This should come from .env
+db_django_user="django"
+db_django_password="admin"
+db_name="db_proyecto_final"
+django_ips="10.0.20.%" # Cambiar este rango por la red donde estarán las máquinas Django
 
 sudo apt update
-sudo apt install mariadb-server
+sudo apt install -y mariadb-server
 unset TMPDIR
 sudo mysql_install_db
+
+echo "Éxito: Instalada MariaDB"
+
 sudo mysql -uroot
-
-db_name="db_proyecto_final"
-
 sudo mysql -u root -e "DROP DATABASE IF EXISTS $db_name;"
 sudo mysql -u root -e "CREATE DATABASE $db_name;"
-sudo mysql -u root $db_name < setup_mariadb.sql
+
+echo "Éxito: Creada la base de datos $db_name"
+
+sudo mysql -u root -e "CREATE USER '$db_django_user'@'%' IDENTIFIED BY '$db_django_password';"
+
+echo "Éxito: Creada la base de datos: [[$db_django_user]] con contraseña [[$db_django_password]]"
+
+sudo mysql -u root -e "GRANT SELECT, INSERT, UPDATE, DELETE ON $db_name.* TO '$db_django_user'@'%';"
+sudo mysql -u root -e "FLUSH PRIVILEGES;"
+
+echo "Éxito: Modificados los permisos para [[$db_django_user]] en la base de datos [[$db_name]]"
+
+sudo sed -i 's/^bind-address.*/bind-address = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf # Esta línea tiene que ser capada para que solo las IPs de los EC2 de Django se permitan
+sudo systemctl restart mariadb
+
+echo "Éxito: Configurada MariaDB para aceptar conexiones remotas"
+
+sleep 2
+
+echo "Éxito, ¡base de datos configurada correctamente!"
