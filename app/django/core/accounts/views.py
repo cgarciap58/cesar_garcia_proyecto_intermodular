97e2d90588a03c1b4374a4b8e069918f09b19149
César García <cgarciap58@iesalbarregas.es>
2026-05-12 08:36:51 +0200

    bugfix

--- app/django/core/accounts/views.py
+++ app/django/core/accounts/views.py
@@ -1,1 +1,10 @@
 # Lógica para la gestión de todas las cuentas
+
+def register_user(request):
+    pass
+
+def login_user(request):
+    pass
+
+def get_user(request):
+    pass
--- app/django/core/urls.py
+++ app/django/core/urls.py
@@ -5,9 +5,9 @@ from .views import test_api, session_test
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/test/', test_api),
-    path('api/auth/register/', register_user),
-    path('api/auth/login/', login_user),
-    path('api/users/me/', get_user),
+    # path('api/auth/register/', register_user),
+    # path('api/auth/login/', login_user),
+    # path('api/users/me/', get_user),
     # path('api/psychologists/'),
     # path('api/appointments/',),
     path('django-debug/session-test/', session_test),
